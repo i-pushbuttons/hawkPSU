e70ac22cac76410c7356d3892997239572631575
i-pushbuttons <themicahlapp@gmail.com>
2024-10-12 17:49:11 -0400

    Initial commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'comp_stem',
+  location: 'us-central1'
+};
+
