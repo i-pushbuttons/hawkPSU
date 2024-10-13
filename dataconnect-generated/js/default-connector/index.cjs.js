67853459568024de6db6dc8555ab41987b1e2591
Jacob Piehl <57500410+jacobpiehl@users.noreply.github.com>
2024-10-13 02:23:48 -0400

    Resolve merge conflicts

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'comp_stem',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
